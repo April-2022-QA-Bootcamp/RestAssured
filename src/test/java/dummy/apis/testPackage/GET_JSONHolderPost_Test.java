4e9a5f68cd79f1179409dbee85d17713442e05d1
mnihussain <md.nasiribnehussain@gmail.com>
2022-09-13 22:36:00 -0400

    Rest Assured Day 1

---
+++ src/test/java/dummy/apis/testPackage/GET_JSONHolderPost_Test.java
@@ -0,0 +1,94 @@
+package dummy.apis.testPackage;
+
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
+
+public class GET_JSONHolderPost_Test {
+
+	@Test(enabled = false)
+	public void printResponse() {
+		RestAssured
+		.when()
+		.get("https://jsonplaceholder.typicode.com/posts")
+		.then()
+		.extract()
+		.response()
+		.prettyPrint();
+	}
+	
+	@Test(enabled = false)
+	public void assertStatusCode() {
+		RestAssured
+			.when()
+			.get("https://jsonplaceholder.typicode.com/posts")
+			.then()
+			.statusCode(201);
+	}
+	
+	@Test(enabled = false)
+	public void logRequestResponse() {
+		RestAssured
+		.given().log().all()
+		.when()
+		.get("https://jsonplaceholder.typicode.com/posts")
+		.then().log().all();
+	}
+	
+	@Test(enabled = false)
+	public void assertion() {
+		RestAssured
+		.when()
+		.get("https://jsonplaceholder.typicode.com/posts/1")
+		.then()
+		.log().all()
+		.body("userId", equalTo(1))
+		.body("id", equalTo(1))
+		.body("title", containsString("sunt aut facere"));
+	}
+	
+	@Test(enabled = false)
+	public void assertContentType() {
+		RestAssured
+		.when()
+		.get("https://jsonplaceholder.typicode.com/posts")
+		.then()
+		.contentType(ContentType.JSON);
+	}
+	
+	@Test(enabled = false)
+	public void printHeaders() {
+		Headers headers =
+		RestAssured
+		.when()
+		.get("https://jsonplaceholder.typicode.com/posts")
+		.then()
+		.extract().headers();
+		
+		System.out.println(headers.toString());
+	}
+	
+	@Test(enabled = false)
+	public void printHeader() {
+		String header =
+		RestAssured
+		.when()
+		.get("https://jsonplaceholder.typicode.com/posts")
+		.then()
+		.extract()
+		.header("Cache-Control");
+		
+		System.out.println(header);
+	}
+	
+	@Test(enabled = true)
+	public void assertAHeader() {
+		RestAssured
+		.when()
+		.get("https://jsonplaceholder.typicode.com/posts")
+		.then()
+		.header("Content-Encoding", equalTo("gzip"));
+	}
+}
